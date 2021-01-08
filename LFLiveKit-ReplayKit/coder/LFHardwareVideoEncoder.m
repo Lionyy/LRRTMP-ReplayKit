77311ebcc99adbc74a52521e69f6c24fc87b393a
RoyLei <leiyang@laka-inc.com>
2021-01-08 09:55:50 +0800

    支持rtmp h265扩展。

---
+++ LFLiveKit-ReplayKit/coder/LFHardwareVideoEncoder.m
@@ -0,0 +1,352 @@
+//
+//  LFHardwareVideoEncoder.m
+//  LFLiveKit
+//
+//  Created by LaiFeng on 16/5/20.
+//  Copyright © 2016年 LaiFeng All rights reserved.
+//
+#import "LFHardwareVideoEncoder.h"
+#import <VideoToolbox/VideoToolbox.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface LFHardwareVideoEncoder (){
+    VTCompressionSessionRef compressionSession;
+    NSInteger frameCount;
+    NSData *vps;
+    NSData *sps;
+    NSData *pps;
+    FILE *fp;
+    BOOL enabledWriteVideoFile;
+    BOOL inResetting;
+}
+
+@property (nonatomic, strong) LFLiveVideoConfiguration *configuration;
+@property (nonatomic, weak) id<LFVideoEncodingDelegate> encoderDelegate;
+@property (nonatomic) NSInteger currentVideoBitRate;
+@property (nonatomic) BOOL isBackGround;
+
+@end
+
+@implementation LFHardwareVideoEncoder
+
+#pragma mark - LifeCycle
+- (instancetype)initWithVideoStreamConfiguration:(LFLiveVideoConfiguration *)configuration {
+    if (self = [super init]) {
+        NSLog(@"USE LFHardwareVideoEncoder");
+        _configuration = configuration;
+        if (_configuration.encoderType == LFVideoH265Encoder) {
+            if ([[AVAssetExportSession allExportPresets] containsObject:AVAssetExportPresetHEVCHighestQuality]) {
+                _configuration.encoderType = LFVideoH265Encoder;
+            }else {
+                _configuration.encoderType = LFVideoH264Encoder;
+            }
+        }
+        [self resetCompressionSession];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(willEnterBackground:) name:UIApplicationWillResignActiveNotification object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(willEnterForeground:) name:UIApplicationDidBecomeActiveNotification object:nil];
+#ifdef DEBUG
+        enabledWriteVideoFile = NO;
+        [self initForFilePath];
+#endif
+        
+    }
+    return self;
+}
+
+- (void)dealloc {
+    if (compressionSession != NULL) {
+        VTCompressionSessionCompleteFrames(compressionSession, kCMTimeInvalid);
+
+        VTCompressionSessionInvalidate(compressionSession);
+        CFRelease(compressionSession);
+        compressionSession = NULL;
+    }
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (BOOL)isSupportPropertyWithSession:(VTCompressionSessionRef)session key:(CFStringRef)key {
+    OSStatus status;
+    static CFDictionaryRef supportedPropertyDictionary;
+    if (!supportedPropertyDictionary) {
+        status = VTSessionCopySupportedPropertyDictionary(session, &supportedPropertyDictionary);
+        if (status != noErr) {
+            return NO;
+        }
+    }
+    BOOL isSupport = [NSNumber numberWithBool:CFDictionaryContainsKey(supportedPropertyDictionary, key)].intValue;
+    return isSupport;
+}
+
+- (void)resetCompressionSession {
+    if (inResetting) {
+        return;
+    }
+    inResetting = YES;
+    if (compressionSession) {
+        VTCompressionSessionCompleteFrames(compressionSession, kCMTimeInvalid);
+        VTCompressionSessionInvalidate(compressionSession);
+        CFRelease(compressionSession);
+        compressionSession = NULL;
+    }
+    
+    CMVideoCodecType codecType = kCMVideoCodecType_H264;
+    if (_configuration.encoderType == LFVideoH264Encoder) {
+        codecType = kCMVideoCodecType_H264;
+    }else if (_configuration.encoderType == LFVideoH265Encoder) {
+        codecType = kCMVideoCodecType_HEVC;
+    }
+    
+    OSStatus status = VTCompressionSessionCreate(NULL, _configuration.videoSize.width, _configuration.videoSize.height, codecType, NULL, NULL, NULL, VideoCompressonOutputCallback, (__bridge void *)self, &compressionSession);
+    if (status != noErr) {
+        return;
+    }
+
+    _currentVideoBitRate = _configuration.videoBitRate;
+    
+    if ([self isSupportPropertyWithSession:compressionSession key:kVTCompressionPropertyKey_MaxKeyFrameInterval]) {
+        VTSessionSetProperty(compressionSession,
+                             kVTCompressionPropertyKey_MaxKeyFrameInterval,
+                             (__bridge CFTypeRef)@(_configuration.videoMaxKeyframeInterval));
+    }
+    
+    if ([self isSupportPropertyWithSession:compressionSession key:kVTCompressionPropertyKey_MaxKeyFrameIntervalDuration]) {
+        VTSessionSetProperty(compressionSession,
+                             kVTCompressionPropertyKey_MaxKeyFrameIntervalDuration,
+                             (__bridge CFTypeRef)@(_configuration.videoMaxKeyframeInterval/_configuration.videoFrameRate));
+    }
+    
+    if ([self isSupportPropertyWithSession:compressionSession key:kVTCompressionPropertyKey_ExpectedFrameRate]) {
+        VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_ExpectedFrameRate, (__bridge CFTypeRef)@(_configuration.videoFrameRate));
+    }
+    
+    if ([self isSupportPropertyWithSession:compressionSession key:kVTCompressionPropertyKey_AverageBitRate]) {
+        VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_AverageBitRate, (__bridge CFTypeRef)@(_configuration.videoBitRate));
+    }
+    
+    if ([self isSupportPropertyWithSession:compressionSession key:kVTCompressionPropertyKey_DataRateLimits]) {
+        NSArray *limit = @[@(_configuration.videoBitRate * 1.5/8), @(1)];
+        VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_DataRateLimits, (__bridge CFArrayRef)limit);
+    }
+
+    if ([self isSupportPropertyWithSession:compressionSession key:kVTCompressionPropertyKey_RealTime]) {
+        VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_RealTime, kCFBooleanTrue);
+    }
+    
+    if ([self isSupportPropertyWithSession:compressionSession key:kVTCompressionPropertyKey_AllowFrameReordering]) {
+        VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_AllowFrameReordering, kCFBooleanFalse);
+    }
+
+    if (_configuration.encoderType == LFVideoH264Encoder) {
+        VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_ProfileLevel, kVTProfileLevel_H264_Main_AutoLevel);
+        VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_H264EntropyMode, kVTH264EntropyMode_CABAC);
+    }else if (_configuration.encoderType == LFVideoH265Encoder) {
+        VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_ProfileLevel, kVTProfileLevel_HEVC_Main_AutoLevel);
+    }
+    
+    VTCompressionSessionPrepareToEncodeFrames(compressionSession);
+    inResetting = NO;
+}
+
+- (void)setVideoBitRate:(NSInteger)videoBitRate {
+    if(_isBackGround) return;
+    
+    if ([self isSupportPropertyWithSession:compressionSession key:kVTCompressionPropertyKey_AverageBitRate]) {
+        VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_AverageBitRate, (__bridge CFTypeRef)@(videoBitRate));
+    }
+    
+    if ([self isSupportPropertyWithSession:compressionSession key:kVTCompressionPropertyKey_DataRateLimits]) {
+        NSArray *limit = @[@(videoBitRate * 1.5/8), @(1)];
+        VTSessionSetProperty(compressionSession, kVTCompressionPropertyKey_DataRateLimits, (__bridge CFArrayRef)limit);
+    }
+    _currentVideoBitRate = videoBitRate;
+}
+
+- (NSInteger)videoBitRate {
+    return _currentVideoBitRate;
+}
+
+#pragma mark - LFVideoEncoder
+
+- (void)encodeVideoData:(CVPixelBufferRef)pixelBuffer timeStamp:(uint64_t)timeStamp {
+    if(_isBackGround) return;
+    frameCount++;
+    if (!compressionSession) {
+        return;
+    }
+    CMTime presentationTimeStamp = CMTimeMake(frameCount, (int32_t)_configuration.videoFrameRate);
+    VTEncodeInfoFlags flags;
+    CMTime duration = CMTimeMake(1, (int32_t)_configuration.videoFrameRate);
+    NSDictionary *properties = nil;
+    if (frameCount % (int32_t)_configuration.videoMaxKeyframeInterval == 0) {
+        properties = @{(__bridge NSString *)kVTEncodeFrameOptionKey_ForceKeyFrame: @YES};
+    }
+    NSNumber *timeNumber = @(timeStamp);
+
+    OSStatus status = VTCompressionSessionEncodeFrame(compressionSession, pixelBuffer, presentationTimeStamp, duration, (__bridge CFDictionaryRef)properties, (__bridge_retained void *)timeNumber, &flags);
+    if(status != noErr){
+        NSLog(@"status != noErr, %d", status);
+        [self resetCompressionSession];
+    }
+}
+
+- (void)stopEncoder {
+    VTCompressionSessionCompleteFrames(compressionSession, kCMTimeIndefinite);
+}
+
+- (void)setDelegate:(id<LFVideoEncodingDelegate>)delegate {
+    _encoderDelegate = delegate;
+}
+
+#pragma mark - Notification
+- (void)willEnterBackground:(NSNotification*)notification {
+    _isBackGround = YES;
+}
+
+- (void)willEnterForeground:(NSNotification*)notification {
+    [self resetCompressionSession];
+    _isBackGround = NO;
+}
+
+#pragma mark - VideoCallBack
+static void VideoCompressonOutputCallback(void *VTref, void *VTFrameRef, OSStatus status, VTEncodeInfoFlags infoFlags, CMSampleBufferRef sampleBuffer){
+    if (!sampleBuffer) return;
+    CFArrayRef array = CMSampleBufferGetSampleAttachmentsArray(sampleBuffer, true);
+    if (!array) return;
+    CFDictionaryRef dic = (CFDictionaryRef)CFArrayGetValueAtIndex(array, 0);
+    if (!dic) return;
+
+    BOOL keyframe = !CFDictionaryContainsKey(dic, kCMSampleAttachmentKey_NotSync);
+    uint64_t timeStamp = [((__bridge_transfer NSNumber *)VTFrameRef) longLongValue];
+
+    LFHardwareVideoEncoder *videoEncoder = (__bridge LFHardwareVideoEncoder *)VTref;
+    if (status != noErr) {
+        return;
+    }
+
+    if (keyframe && !videoEncoder->sps) {
+        CMFormatDescriptionRef format = CMSampleBufferGetFormatDescription(sampleBuffer);
+
+        if (videoEncoder.configuration.encoderType == LFVideoH264Encoder) {
+            size_t sparameterSetSize, sparameterSetCount;
+            const uint8_t *sparameterSet;
+            OSStatus statusCode = CMVideoFormatDescriptionGetH264ParameterSetAtIndex(format, 0, &sparameterSet, &sparameterSetSize, &sparameterSetCount, 0);
+            if (statusCode == noErr) {
+                size_t pparameterSetSize, pparameterSetCount;
+                const uint8_t *pparameterSet;
+                OSStatus statusCode = CMVideoFormatDescriptionGetH264ParameterSetAtIndex(format, 1, &pparameterSet, &pparameterSetSize, &pparameterSetCount, 0);
+                if (statusCode == noErr) {
+                    videoEncoder->sps = [NSData dataWithBytes:sparameterSet length:sparameterSetSize];
+                    videoEncoder->pps = [NSData dataWithBytes:pparameterSet length:pparameterSetSize];
+
+                    if (videoEncoder->enabledWriteVideoFile) {
+                        NSMutableData *data = [[NSMutableData alloc] init];
+                        uint8_t header[] = {0x00, 0x00, 0x00, 0x01};
+                        [data appendBytes:header length:4];
+                        [data appendData:videoEncoder->sps];
+                        [data appendBytes:header length:4];
+                        [data appendData:videoEncoder->pps];
+                        fwrite(data.bytes, 1, data.length, videoEncoder->fp);
+                    }
+                }// pps
+            }// sps
+        }
+        else if (videoEncoder.configuration.encoderType == LFVideoH265Encoder) {
+            const uint8_t *vps;
+            size_t vpsSize;
+            int NALUnitHeaderLengthOut;
+            size_t parmCount;
+            OSStatus statusCode = CMVideoFormatDescriptionGetHEVCParameterSetAtIndex(format, 0, &vps, &vpsSize, &parmCount, &NALUnitHeaderLengthOut);
+            if (statusCode == noErr) {
+                const uint8_t *sps;
+                size_t spsSize;
+                statusCode = CMVideoFormatDescriptionGetHEVCParameterSetAtIndex(format, 1, &sps, &spsSize, &parmCount, &NALUnitHeaderLengthOut);
+                if (statusCode == noErr) {
+                    const uint8_t *pps;
+                    size_t ppsSize;
+                    statusCode = CMVideoFormatDescriptionGetHEVCParameterSetAtIndex(format, 2, &pps, &ppsSize, &parmCount, &NALUnitHeaderLengthOut);
+                    if (statusCode == noErr) {
+                        videoEncoder->vps = [NSData dataWithBytes:vps length:vpsSize];
+                        videoEncoder->sps = [NSData dataWithBytes:sps length:spsSize];
+                        videoEncoder->pps = [NSData dataWithBytes:pps length:ppsSize];
+                        
+                        if (videoEncoder->enabledWriteVideoFile) {
+                            NSMutableData *data = [[NSMutableData alloc] init];
+                            uint8_t header[] = {0x00, 0x00, 0x00, 0x01};
+                            [data appendBytes:header length:4];
+                            [data appendData:videoEncoder->vps];
+                            [data appendBytes:header length:4];
+                            [data appendData:videoEncoder->sps];
+                            [data appendBytes:header length:4];
+                            [data appendData:videoEncoder->pps];
+                            fwrite(data.bytes, 1, data.length, videoEncoder->fp);
+                        }
+                    }// pps
+                }// sps
+            }// vps
+        }// h265
+    }
+
+    CMBlockBufferRef dataBuffer = CMSampleBufferGetDataBuffer(sampleBuffer);
+    size_t length, totalLength;
+    char *dataPointer;
+    OSStatus statusCodeRet = CMBlockBufferGetDataPointer(dataBuffer, 0, &length, &totalLength, &dataPointer);
+    if (statusCodeRet == noErr) {
+        size_t bufferOffset = 0;
+        static const int AVCCHeaderLength = 4;
+        while (bufferOffset < totalLength - AVCCHeaderLength) {
+            // Read the NAL unit length
+            uint32_t NALUnitLength = 0;
+            memcpy(&NALUnitLength, dataPointer + bufferOffset, AVCCHeaderLength);
+
+            NALUnitLength = CFSwapInt32BigToHost(NALUnitLength);
+
+            LFVideoFrame *videoFrame = [LFVideoFrame new];
+            videoFrame.timestamp = timeStamp;
+            videoFrame.data = [[NSData alloc] initWithBytes:(dataPointer + bufferOffset + AVCCHeaderLength) length:NALUnitLength];
+            videoFrame.isKeyFrame = keyframe;
+            videoFrame.vps = videoEncoder->vps;
+            videoFrame.sps = videoEncoder->sps;
+            videoFrame.pps = videoEncoder->pps;
+            
+            if (videoEncoder.encoderDelegate && [videoEncoder.encoderDelegate respondsToSelector:@selector(videoEncoder:videoFrame:)]) {
+                [videoEncoder.encoderDelegate videoEncoder:videoEncoder videoFrame:videoFrame];
+            }
+
+            if (videoEncoder->enabledWriteVideoFile) {
+                NSMutableData *data = [[NSMutableData alloc] init];
+                if (keyframe) {
+                    uint8_t header[] = {0x00, 0x00, 0x00, 0x01};
+                    [data appendBytes:header length:4];
+                } else {
+                    uint8_t header[] = {0x00, 0x00, 0x01};
+                    [data appendBytes:header length:3];
+                }
+                [data appendData:videoFrame.data];
+
+                fwrite(data.bytes, 1, data.length, videoEncoder->fp);
+            }
+
+            bufferOffset += AVCCHeaderLength + NALUnitLength;
+        }
+    }
+}
+
+#pragma mark - Debug
+
+- (void)initForFilePath {
+    NSString *path = [self GetFilePathByfileName:@"IOSCamDemo.h264"];
+    if (_configuration.encoderType == LFVideoH265Encoder) {
+        path = [self GetFilePathByfileName:@"IOSCamDemo.h265"];
+    }
+    NSLog(@"%@", path);
+    self->fp = fopen([path cStringUsingEncoding:NSUTF8StringEncoding], "wb");
+}
+
+- (NSString *)GetFilePathByfileName:(NSString*)filename {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *writablePath = [documentsDirectory stringByAppendingPathComponent:filename];
+    return writablePath;
+}
+
+@end
---
+++ samples/LFLiveKitDemo/Recoder/ZFUploadTool.m
@@ -0,0 +1,145 @@
+//
+//  ZFUploadTool.m
+//  Recoder
+//
+//  Created by 张帆 on 2018/12/6.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+#import "ZFUploadTool.h"
+#import <LFLiveKit.h>
+
+inline static NSString *formatedSpeed(float bytes, float elapsed_milli) {
+    if (elapsed_milli <= 0) {
+        return @"N/A";
+    }
+    if (bytes <= 0) {
+        return @"0 KB/s";
+    }
+    float bytes_per_sec = ((float)bytes) * 1000.f /  elapsed_milli;
+    if (bytes_per_sec >= 1000 * 1000) {
+        return [NSString stringWithFormat:@"%.2f MB/s", ((float)bytes_per_sec) / 1000 / 1000];
+    } else if (bytes_per_sec >= 1000) {
+        return [NSString stringWithFormat:@"%.1f KB/s", ((float)bytes_per_sec) / 1000];
+    } else {
+        return [NSString stringWithFormat:@"%ld B/s", (long)bytes_per_sec];
+    }
+}
+
+
+@interface ZFUploadTool () <LFLiveSessionDelegate>
+
+@property (nonatomic, strong) LFLiveDebug *debugInfo;
+@property (nonatomic, strong) LFLiveSession *session;
+@property (nonatomic, assign) BOOL mic;
+@property (nonatomic, assign) int frameQuality;
+@property (nonatomic, copy) NSString *url;
+
+@end
+
+@implementation ZFUploadTool
+
++ (instancetype)shareTool {
+    static dispatch_once_t onceToken;
+    static ZFUploadTool *tool = nil;
+    dispatch_once(&onceToken, ^{
+        tool = [[ZFUploadTool alloc] init];
+    });
+    return tool;
+}
+
+- (void)prepareToStart:(NSDictionary *)dict {
+    _url = dict[@"endpointURL"];
+    _mic = [dict[@"mic"] boolValue];
+    _mic = YES;
+    _frameQuality = [dict[@"frameQuality"] intValue]; // 0 高 1中 2低
+    if (!_url) {
+        _url = @"rtmp://192.168.45.174/live/123";
+    }
+    [self lf];
+}
+- (void)lf {
+    LFLiveStreamInfo *stream = [LFLiveStreamInfo new];
+    stream.url = _url;
+//    stream.videoConfiguration = [LFLiveVideoConfiguration defaultConfigurationForQuality:LFLiveVideoQuality_High3 outputImageOrientation:UIInterfaceOrientationLandscapeRight];
+//    stream.audioConfiguration = [LFLiveAudioConfiguration defaultConfigurationForQuality:LFLiveAudioQuality_Medium];
+//    stream.videoConfiguration.videoSize = CGSizeMake(UIScreen.mainScreen.bounds.size.height, UIScreen.mainScreen.bounds.size.width);
+    
+    [self.session startLive:stream];
+}
+
+#pragma mark -- Getter Setter
+- (LFLiveSession *)session {
+    if (_session == nil) {
+        
+        LFLiveAudioConfiguration *audioConfiguration = [LFLiveAudioConfiguration defaultConfigurationForQuality:LFLiveAudioQuality_High];
+        audioConfiguration.numberOfChannels = 1;
+        LFLiveVideoConfiguration *videoConfiguration;
+       
+        videoConfiguration = [LFLiveVideoConfiguration defaultConfigurationForQuality:LFLiveVideoQuality_High4];
+        videoConfiguration.videoSize = CGSizeMake((NSInteger)(UIScreen.mainScreen.bounds.size.width/UIScreen.mainScreen.bounds.size.height * 1920), 1920);
+        videoConfiguration.encoderType = LFVideoH265Encoder;
+        
+        _session = [[LFLiveSession alloc] initWithAudioConfiguration:audioConfiguration videoConfiguration:videoConfiguration captureType:_mic? LFLiveInputMaskAll:LFLiveInputMaskVideo];
+        
+        _session.delegate = self;
+        _session.showDebugInfo = YES;
+        
+    }
+    return _session;
+}
+
+-(void)stop {
+    [self.session stopLive];
+}
+
+
+- (void)sendAudioBuffer:(CMSampleBufferRef)sampleBuffer {
+    if (_mic) {
+        [self.session pushAudioBuffer:sampleBuffer];
+    }
+    
+}
+
+- (void)sendVideoBuffer:(CMSampleBufferRef)sampleBuffer {
+    [self.session pushVideoBuffer:sampleBuffer];
+}
+
+#pragma mark -- LFStreamingSessionDelegate
+/** live status changed will callback */
+- (void)liveSession:(nullable LFLiveSession *)session liveStateDidChange:(LFLiveState)state {
+//    NSLog(@"liveStateDidChange: %ld", state);
+    switch (state) {
+        case LFLiveReady:
+            NSLog(@"未连接");
+            break;
+        case LFLivePending:
+            NSLog(@"连接中");
+            break;
+        case LFLiveStart:
+            NSLog(@"已连接");
+            break;
+        case LFLiveError:
+            NSLog(@"连接错误");
+            break;
+        case LFLiveStop:
+            NSLog(@"未连接");
+            break;
+        default:
+            break;
+    }
+}
+
+/** live debug info callback */
+- (void)liveSession:(nullable LFLiveSession *)session debugInfo:(nullable LFLiveDebug *)debugInfo {
+//    NSString *speed = formatedSpeed(debugInfo.currentBandwidth, debugInfo.elapsedMilli);
+    
+}
+/** callback socket errorcode */
+- (void)liveSession:(nullable LFLiveSession *)session errorCode:(LFLiveSocketErrorCode)errorCode {
+    NSLog(@"errorCode: %ld", errorCode);
+}
+
+
+
+@end
